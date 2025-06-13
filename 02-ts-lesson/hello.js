a3d6270be62e7823b13f8dbf916e15d605055efd
Pratap Padhy <pratappadhy2005@gmail.com>
2025-06-13 17:15:14 +1000

    -Added type script

---
+++ 02-ts-lesson/hello.js
@@ -0,0 +1,15 @@
+var hello = 100;
+var myString = "hello";
+//let largeNumber:BigInt = BigInt(465)
+var isActive = true;
+//const uniqueKey: symbol = Symbol("unique")
+var obj = {};
+var emptyValue = null;
+var unInitilised;
+var person = {
+    name: "pratappadhy",
+    age: 30,
+    isActive: true,
+    //uniqueKey: Symbol("unique")
+};
+console.log(hello);
---
+++ 02-ts-lesson/hello.ts
@@ -0,0 +1,26 @@
+let hello:number = 100
+let myString:string = "hello"
+//let largeNumber:BigInt = BigInt(465)
+
+const isActive: boolean = true
+
+//const uniqueKey: symbol = Symbol("unique")
+const obj = {}
+
+const emptyValue: null = null;
+
+let unInitilised: undefined;
+
+const person: {
+    name: string,
+    age: number,
+    isActive: boolean,
+    //uniqueKey: symbol
+} = {
+    name: "pratappadhy",
+    age: 30,
+    isActive: true,
+    //uniqueKey: Symbol("unique")
+}
+
+console.log(hello)
