ac45f2c4629dffb72737c3de55e69ae2ea40c7a7
Pratap Padhy <pratappadhy2005@gmail.com>
2026-01-17 20:11:37 +1100

    -Added sample project with basic setup

---
+++ 03-Project/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div id="dad">
+        <h1 id="child1">Hello World</h1>
+        <p id="child2">Welcome to React</p>
+      </div>
+      <div id="chacha">
+        <h1 id="child3">Hello World</h1>
+        <p id="child4">Welcome to React</p>
+      </div>
+    </>
+
+  )
+}
+
+export default App
+
