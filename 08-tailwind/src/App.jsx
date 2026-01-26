ccb129e0f80398aa231bd82809ca3763fd5e927e
Pratap Padhy <pratappadhy2005@gmail.com>
2026-01-26 19:48:45 +1100

    -Added Tailwind utlity first plugin to enable tailwind

---
+++ 08-tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="bg-red-500 h-screen w-screen flex items-center justify-center">
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
