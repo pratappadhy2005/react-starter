4963f83edf6252e057d07025e5243672bafa2125
Pratap Padhy <pratappadhy2005@gmail.com>
2026-03-29 22:07:14 +1100

    -props concept added

--- 11-End-To-End/03-reatcjs-concepts/src/App.jsx
+++ 11-End-To-End/03-reatcjs-concepts/src/App.jsx
@@ -3,6 +3,26 @@ import ClassBasedComponent from './components/class-based-component.jsx'
 import FunctionalComponent from './components/functional-component.jsx'
 import ProductList from './components/products/index.jsx'
 
+const dummyProductData = [
+  {
+    id: 1,
+    name: 'Product 1',
+    price: 100,
+    quantity: 10,
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+    price: 200,
+    quantity: 20,
+  },
+  {
+    id: 3,
+    name: 'Product 3',
+    price: 300,
+    quantity: 30,
+  },
+]
 function App() {
 
   return (
@@ -10,7 +30,7 @@ function App() {
       <h1>React JS Concepts 2026</h1>
       {/* <ClassBasedComponent />
       <FunctionalComponent /> */}
-      <ProductList />
+      <ProductList dummyProductData={dummyProductData} />
     </div>
   )
 }
--- 11-End-To-End/03-reatcjs-concepts/src/components/products/index.jsx
+++ 11-End-To-End/03-reatcjs-concepts/src/components/products/index.jsx
@@ -1,32 +1,10 @@
 import ProductItem from './components/product-item.jsx'
 
-const dummyProductData = [
-    {
-        id: 1,
-        name: 'Product 1',
-        price: 100,
-        quantity: 10,
-    },
-    {
-        id: 2,
-        name: 'Product 2',
-        price: 200,
-        quantity: 20,
-    },
-    {
-        id: 3,
-        name: 'Product 3',
-        price: 300,
-        quantity: 30,
-    },
-]
-
-function ProductList() {
+function ProductList(props) {
     return (
         <div>
-            <h3>ECommerce Project</h3>
-            {
-                dummyProductData.map((product, index) => (
+            <h3>ECommerce Project</h3>{
+                props.dummyProductData.map((product, index) => (
                     <ProductItem key={index} product={product} />
                 ))
             }
