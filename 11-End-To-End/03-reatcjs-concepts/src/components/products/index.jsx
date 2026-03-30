c04ec2fb675e9b547f9d77b652072e204d1a5545
Pratap Padhy <pratappadhy2005@gmail.com>
2026-03-30 19:15:49 +1100

    -Added conditional statements

--- 11-End-To-End/03-reatcjs-concepts/src/components/products/index.jsx
+++ 11-End-To-End/03-reatcjs-concepts/src/components/products/index.jsx
@@ -2,9 +2,23 @@ import ProductItem from './components/product-item.jsx'
 import "./style.css"
 
 function ProductList(props) {
+
+    const flag = true;
+
+    function renderTextBlock(getFlag) {
+        return getFlag ? <h4>Product List</h4> : <h4>No Product List</h4>
+    }
+
+    let textBlock = renderTextBlock(flag)
+
     return (
         <div>
-            <h3>ECommerce Project</h3>{
+            <h3>ECommerce Project</h3>
+            {
+                textBlock
+            }
+
+            {
                 props.dummyProductData.map((product, index) => (
                     <ProductItem key={index} product={product} />
                 ))
