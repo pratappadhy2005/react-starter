75d02ad24e94fe943e61809ccbc8dea888f811bf
Pratap Padhy <pratappadhy2005@gmail.com>
2026-04-04 22:01:46 +1100

    -React basics with components

---
+++ 11-End-To-End/react-demos/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MainContent = () => {
+    return (
+        <main>
+            <h2>Main Content</h2>
+            <p>Welcome to the main content of the website</p>
+        </main>
+    )
+}
+
+export default MainContent
