6d1662239eae6143bbc09ba62949f7d3b352030b
Pratap Padhy <pratappadhy2005@gmail.com>
2026-04-05 20:11:46 +1000

    -Next assignment to create a functional component

--- 11-End-To-End/react-demos/src/App.jsx
+++ 11-End-To-End/react-demos/src/App.jsx
@@ -1,14 +1,11 @@
 import Header from "./components/Header"
 import MainContent from "./components/MainContent"
 import Footer from "./components/Footer"
+import WelcomeMessage from "./components/WelcomeMessage"
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
+    <WelcomeMessage />
   )
 }
 
---
+++ 11-End-To-End/react-demos/src/components/WelcomeMessage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const WelcomeMessage = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p className="text">
+                Welcome learning JSX!
+            </p>
+        </div>
+    )
+}
+
+export default WelcomeMessage   
