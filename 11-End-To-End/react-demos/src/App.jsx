ecef267bd432e3e21e6ac1b0597d2970d15ab00e
Pratap Padhy <pratappadhy2005@gmail.com>
2026-04-13 21:52:44 +1000

    -Added react useState hook

--- 11-End-To-End/react-demos/src/App.jsx
+++ 11-End-To-End/react-demos/src/App.jsx
@@ -2,12 +2,17 @@ import ProfileCard from "./components/styledemo/ProfileCard"
 import StyleCard from "./components/styledemo/StyleCard"
 import EventDemo from "./components/eventdemo/EventDemo"
 import Copy from "./components/eventdemo/Copy"
-
+import { useState } from "react"
 const App = () => {
+  const [count, setCount] = useState(0)
+  console.log(count);
+
   return (
     <div>
-      <EventDemo />
-      <Copy />
+      <h1>Count: {count}</h1>
+      <button style={{ padding: '10px 20px' }} onClick={() => setCount(count + 1)}>+</button><br />
+      <br />
+      <button style={{ padding: '10px 20px' }} onClick={() => setCount(count - 1)}>-</button>
     </div>
   )
 }
