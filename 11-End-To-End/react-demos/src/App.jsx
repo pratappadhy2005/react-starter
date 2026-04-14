521b4ce02cc63e8e7dc928a74d23151862e63d07
Pratap Padhy <pratappadhy2005@gmail.com>
2026-04-14 21:36:54 +1000

    -More examples of object  react useState hook

--- 11-End-To-End/react-demos/src/App.jsx
+++ 11-End-To-End/react-demos/src/App.jsx
@@ -2,6 +2,11 @@ import { useState } from "react"
 const App = () => {
   const [count, setCount] = useState(0)
   console.log(count);
+  const [movie, setMovie] = useState({
+    name: 'Avatar',
+    director: 'James Cameron',
+    year: 2009,
+  })
 
   //Array Destructuring
   const [names, setNames] = useState(['Pratappadhy', 'Pratap', 'Padhy'])
@@ -24,6 +29,13 @@ const App = () => {
       <button style={{ padding: '10px 20px' }} onClick={() => setNames(names.filter((name) => name !== 'Pratap'))}>Remove Name</button>
       <br />
       <button style={{ padding: '10px 20px' }} onClick={() => setNames(names.map((name) => (name === 'Padhy' ? 'Prayan' : name)))}>Update One Friend</button>
+
+      <h1>Movie Name: {movie.name}</h1>
+      <h1>Movie Director: {movie.director}</h1>
+      <h1>Movie Year: {movie.year}</h1>
+      <button style={{ padding: '10px 20px' }} onClick={() => setMovie({ ...movie, name: 'Avatar 2' })}>Update Movie Name</button>
+      <button style={{ padding: '10px 20px' }} onClick={() => setMovie({ ...movie, director: 'Pratap Padhy' })}>Update Movie Director</button>
+      <button style={{ padding: '10px 20px' }} onClick={() => setMovie({ ...movie, year: 2010 })}>Update Movie Year</button>
     </div>
   )
 }
