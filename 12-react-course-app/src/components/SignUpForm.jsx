1555283ab54bedfebd5f9c600d81f2fb84852794
Pratap Padhy <pratappadhy2005@gmail.com>
2026-04-29 22:11:00 +1000

    -Form validation using react hook form

--- 12-react-course-app/src/components/SignUpForm.jsx
+++ 12-react-course-app/src/components/SignUpForm.jsx
@@ -1,24 +1,26 @@
 import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 const SignUpForm = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const { register, handleSubmit, formState } = useForm()
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        alert(`Email: ${email}, Password: ${password}`)
+    const onSubmit = (data) => {
+        alert(`Username: ${data.username}, Password: ${data.password}`)
     }
+
     return (
         <div style={{ maxWidth: '400px', margin: '2rem auto' }}>
             <h1>Sign Up Form</h1>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div style={{ marginBottom: '10px' }}>
                     <label htmlFor="username">Username:</label>
-                    <input type="text" id="username" name="username" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input type="text" id="username" name="username" {...register('username', { required: "Username is required", minLength: { value: 8, message: "Username must be at least 8 characters long" } })} />
+                    {formState.errors.username?.message && <p style={{ color: 'red' }}>{formState.errors.username.message}</p>}
                 </div>
                 <div style={{ marginBottom: '10px' }}>
                     <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <input type="password" id="password" name="password" {...register('password', { required: "Password is required", minLength: { value: 8, message: "Password must be at least 8 characters long" } })} />
+                    {formState.errors.password?.message && <p style={{ color: 'red' }}>{formState.errors.password.message}</p>}
                 </div>
                 <div style={{ marginBottom: '10px' }}>
                     <input type="submit" value="Sign Up" />
