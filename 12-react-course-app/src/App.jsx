80776b27eeab5451647c5fb23d7bec7a474d043a
Pratap Padhy <pratappadhy2005@gmail.com>
2026-04-30 22:32:34 +1000

    -Added react router dom

--- 12-react-course-app/src/App.jsx
+++ 12-react-course-app/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import SignUpForm from './components/SignUpForm'
+import { Route, Routes, Link } from 'react-router-dom'
 
 function Greeting({ name, age }) {
   return (
@@ -8,10 +9,56 @@ function Greeting({ name, age }) {
     </div>
   )
 }
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  )
+}
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  )
+}
+
+function NotFound() {
+  return (
+    <div>
+      <h1>404</h1>
+    </div>
+  )
+}
+
+function Footer() {
+  return (
+    <div>
+      <p>Footer</p>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div>
-      <SignUpForm />
+      <nav style={{ display: 'flex', justifyContent: 'center' }}>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+      <div>
+        <Footer />
+      </div>
     </div>
   )
 }
